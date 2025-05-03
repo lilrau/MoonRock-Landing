4f2959ab2e86399ba95593e966914ab37505fe7f
Raul Souza <raulsscoc@hotmail.com>
2025-05-03 13:14:54 -0300

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'MoonRock Soluções',
+  description: 'Uma plataforma completa e personalizada de gerenciamento de negócios que ajuda equipes a colaborar, monitorar o progresso e entregar resultados mais rapidamente.',
 }
 
 export default function RootLayout({
